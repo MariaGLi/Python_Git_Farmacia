a0df3696e6df369ca3841bf4208ce5259070c586
MariaGLi <lizarazoleal27@gmail.com>
2024-08-13 11:18:24 -0500

    ajuste

--- python.py
+++ python.py
@@ -1,5 +1,15 @@
 import json
+import datetime
 from os import system
+
+with open("Compras.json", encoding="utf-8") as file:
+    compra=json.load(file)
+
+with open("Empleados.json", encoding="utf-8") as file2:
+    empleados=json.load(file2)
+
+with open("Medicamentos.json", encoding="utf-8") as file3:
+    medicamento=json.load(file3)
 
 with open("Pacientes.json", encoding="utf-8") as file4:
     pacientes=json.load(file4)
