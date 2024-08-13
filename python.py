6c3ecbdbe712a97bb6d1c89808f817bd9092f7c8
MariaGLi <lizarazoleal27@gmail.com>
2024-08-13 11:10:48 -0500

    Creación de los otros Jsons

--- python.py
+++ python.py
@@ -1,2 +1,11 @@
 import json
 from os import system
+
+with open("Pacientes.json", encoding="utf-8") as file4:
+    pacientes=json.load(file4)
+
+with open("Proveedores.json", encoding="utf-8") as file5:
+    proveedor=json.load(file5)
+
+with open("Ventas.json", encoding="utf-8") as file6:
+    venta=json.load(file6)
